079f9ca4e0c8bb86c8041a6fdff5fd56c253a9da
pengshengsong <shengsongpeng@huangbaoche.com>
2016-10-20 15:18:45 +0800

    初始化代码

---
+++ 第三方程序打开文件/第三方程序打开文件/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  第三方程序打开文件
+//
+//  Created by 彭盛凇 on 16/10/20.
+//  Copyright © 2016年 huangbaoche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
